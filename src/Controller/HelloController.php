99c4c2045024ab70de7e111c28f705cceb9f7f9d
Hugo Hamon <hello@kodero.fr>
2023-02-20 11:27:46 -0500

    Add hello action

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    #[Route('/hello/{name}', name: 'app_hello')]
+    public function index(Request $request, string $name): Response
+    {
+        $firstName = $request->query->get('firstName');
+        $lastName = $request->query->get('lastName');
+
+        return $this->render('hello/index.html.twig', [
+            'first_name' => $name,
+            'last_name' => $lastName,
+        ]);
+    }
+}
