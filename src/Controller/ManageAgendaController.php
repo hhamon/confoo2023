40fdcc5eaf5dc1d3180e25b580878651f289f654
Hugo Hamon <hello@kodero.fr>
2023-02-21 14:28:52 -0500

    Use `IsGranted` attribute on controller

--- src/Controller/ManageAgendaController.php
+++ src/Controller/ManageAgendaController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use App\Entity\Agenda;
@@ -8,19 +10,19 @@ use App\Security\Voter\AgendaVoter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/agendas/{slug}/manage', name: 'app_manage_agenda', methods: ['GET', 'POST'])]
-class ManageAgendaController extends AbstractController
+final class ManageAgendaController extends AbstractController
 {
     public function __construct(
         private readonly AgendaSlotRepository $agendaSlotRepository,
     ) {
     }
 
+    #[Route('/agendas/{slug}/manage', name: 'app_manage_agenda', methods: ['GET', 'POST'])]
+    #[IsGranted(attribute: AgendaVoter::EDIT, subject: 'agenda')]
     public function __invoke(Agenda $agenda): Response
     {
-        $this->denyAccessUnlessGranted(AgendaVoter::EDIT, $agenda);
-
         return $this->render('agenda/agenda.html.twig', [
             'agenda' => $agenda,
             'upcoming_slots' => $this->agendaSlotRepository->findUpcomingSlots($agenda),
